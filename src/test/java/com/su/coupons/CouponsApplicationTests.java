f26ceb8b6242112be5e78723ae88f1d6393e502f
su <2410973613@qq.com>
2025-02-17 17:10:24 +0800

    初始

---
+++ src/test/java/com/su/coupons/CouponsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.su.coupons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
